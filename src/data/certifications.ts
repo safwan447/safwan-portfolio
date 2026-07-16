9b81c89ecc674166844f5c0a80cad131b679c294
Safwan <mohammedsafwan12000@gmail.com>
2026-07-16 13:51:28 +0530

    Update certification certificate links

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -5,11 +5,11 @@ export const certifications: Certification[] = [
     name: "AWS Generative AI Fundamentals",
     issuer: "Amazon Web Services (AWS)",
     date: "June 2025",
-    credentialUrl:
+    certificateUrl:
       "https://www.credly.com/badges/0eda3d46-1f54-46a8-8f3f-3e5f0433cae8/linked_in_profile",
   },
   {
-    name: "AWS Solutions Architecture Simulation",
+    name: "AWS - Solutions Architecture Job Simulation",
     issuer: "Amazon Web Services (AWS)",
     date: "July 2025",
     certificateUrl:
